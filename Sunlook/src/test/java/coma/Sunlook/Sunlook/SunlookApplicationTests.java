ef68b8700d9c6b8288bd7bb1f013871d85066973
jumok <jumokcom2001@gmail.com>
2024-10-03 21:24:18 +0900

    project start

---
+++ Sunlook/src/test/java/coma/Sunlook/Sunlook/SunlookApplicationTests.java
@@ -0,0 +1,13 @@
+package coma.Sunlook.Sunlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
